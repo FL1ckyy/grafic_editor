bf5e15374c0249c8bcd0a8f4e8d65a9bc453ab6b
fl1ckyyyy <gagagaggff@gmail.com>
2026-02-05 05:52:07 +0300

    Создание классов фигур

---
+++ grafic_editor/Eclipse.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System;
+
+namespace VectorEditor
+{
+    public class Ellipse : Shape
+    {
+        public override void Draw(Graphics graphics)
+        {
+            int x = Math.Min(startPoint.X, endPoint.X);
+            int y = Math.Min(startPoint.Y, endPoint.Y);
+            int width = Math.Abs(endPoint.X - startPoint.X);
+            int height = Math.Abs(endPoint.Y - startPoint.Y);
+
+            using (Pen pen = new Pen(color, 2))
+            using (Brush brush = new SolidBrush(Color.FromArgb(50, color)))
+            {
+                graphics.FillEllipse(brush, x, y, width, height);
+                if (isSelected)
+                {
+                    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                }
+                graphics.DrawEllipse(pen, x, y, width, height);
+            }
+        }
+
+        public override bool Contains(Point point)
+        {
+            int x = Math.Min(startPoint.X, endPoint.X);
+            int y = Math.Min(startPoint.Y, endPoint.Y);
+            int width = Math.Abs(endPoint.X - startPoint.X);
+            int height = Math.Abs(endPoint.Y - startPoint.Y);
+
+            float centerX = x + width / 2.0f;
+            float centerY = y + height / 2.0f;
+            float dx = point.X - centerX;
+            float dy = point.Y - centerY;
+
+            return (dx * dx) / (width * width / 4.0f) +
+                   (dy * dy) / (height * height / 4.0f) <= 1;
+        }
+
+        public override void Move(int deltaX, int deltaY)
+        {
+            startPoint = new Point(startPoint.X + deltaX, startPoint.Y + deltaY);
+            endPoint = new Point(endPoint.X + deltaX, endPoint.Y + deltaY);
+        }
+    }
+}
---
+++ grafic_editor/Line.cs
@@ -0,0 +1,66 @@
+﻿using System.Drawing;
+using System;
+
+namespace VectorEditor
+{
+    public class Line : Shape
+    {
+        public override void Draw(Graphics graphics)
+        {
+            using (Pen pen = new Pen(color, 2))
+            {
+                if (isSelected)
+                {
+                    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                }
+                graphics.DrawLine(pen, startPoint, endPoint);
+            }
+        }
+
+        public override bool Contains(Point point)
+        {
+            float distance = DistanceToLine(point, startPoint, endPoint);
+            return distance < 5;
+        }
+
+        public override void Move(int deltaX, int deltaY)
+        {
+            startPoint = new Point(startPoint.X + deltaX, startPoint.Y + deltaY);
+            endPoint = new Point(endPoint.X + deltaX, endPoint.Y + deltaY);
+        }
+
+        private float DistanceToLine(Point point, Point lineStart, Point lineEnd)
+        {
+            float A = point.X - lineStart.X;
+            float B = point.Y - lineStart.Y;
+            float C = lineEnd.X - lineStart.X;
+            float D = lineEnd.Y - lineStart.Y;
+
+            float dot = A * C + B * D;
+            float lenSq = C * C + D * D;
+            float param = (lenSq != 0) ? dot / lenSq : -1;
+
+            float xx, yy;
+
+            if (param < 0)
+            {
+                xx = lineStart.X;
+                yy = lineStart.Y;
+            }
+            else if (param > 1)
+            {
+                xx = lineEnd.X;
+                yy = lineEnd.Y;
+            }
+            else
+            {
+                xx = lineStart.X + param * C;
+                yy = lineStart.Y + param * D;
+            }
+
+            float dx = point.X - xx;
+            float dy = point.Y - yy;
+            return (float)Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
